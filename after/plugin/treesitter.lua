2909e3a5375abf54e128b8e4624596df116ffb7c
sai-koushik-macha <saikoushikmacha@gmail.com>
2024-06-17 20:53:14 +0530

    playground been archived

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -12,23 +12,4 @@ require("nvim-treesitter.configs").setup({
     },
 
     indent = { enable = true, disable = { "yaml" } },
-
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25,
-        persist_queries = false,
-        keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?",
-        },
-    },
 })
