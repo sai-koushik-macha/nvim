e05d5d96b2c6f2ba0fba6c6fd7a5f5863e64140d
sai-koushik-macha <saikoushikmacha@gmail.com>
2024-06-22 11:20:06 +0530

    Use clang-format

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,12 @@
+local autocmd = require("macha.autocmds").autocmd
+local group = require("macha.autocmds").group("CLAGFORMAT")
+
+
+autocmd("BufWritePost", {
+    group = group,
+    pattern = "*",
+    callback = function()
+        vim.cmd(":!clang-format -i %")
+        -- vim.cmd("!echo 'Hi'")
+    end,
+})
