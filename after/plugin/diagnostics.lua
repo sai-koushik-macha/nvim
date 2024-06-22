78e74efe9e09b5739ddc34a3cdd4c575c9e22105
sai-koushik-macha <saikoushikmacha@gmail.com>
2024-06-22 10:33:57 +0530

    Add the diagnostic

--- after/plugin/diagnostics.lua
+++ after/plugin/diagnostics.lua
@@ -1,5 +1,34 @@
 local diagnostic = vim.diagnostic
 
+diagnostic.config({
+  virtual_text = {
+    source = "always",  -- Or "if_many"
+  },
+  float = {
+    source = "always",  -- Or "if_many"
+  },
+})
+
+local autocmd = require("macha.autocmds").autocmd
+local group = require("macha.autocmds").group("VIM-DIAGNOSTIC")
+
+autocmd("CursorHold", {
+    group = group,
+    buffer = bufnr,
+    callback = function()
+        local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = 'rounded',
+            source = 'always',
+            prefix = ' ',
+            scope = 'cursor',
+        }
+        vim.diagnostic.open_float(nil, opts)
+    end
+})
+
+
 -- Keymaps
 local nmap = require("macha.remap").nmap
 
