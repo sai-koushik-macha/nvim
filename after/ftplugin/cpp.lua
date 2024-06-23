9f8692a880687ecb385a23093212b9eca74ebe29
sai-koushik-macha <saikoushikmacha@gmail.com>
2024-06-23 07:15:30 +0530

    Formatting change in ftplugin/cpp.lua

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,12 +1,11 @@
 local autocmd = require("macha.autocmds").autocmd
 local group = require("macha.autocmds").group("CLAGFORMAT")
 
-
-autocmd("BufWritePost", {
+autocmd("BufWritePost",
+{
     group = group,
     pattern = "*",
     callback = function()
         vim.cmd(":silent !clang-format -i %")
-        -- vim.cmd("!echo 'Hi'")
     end,
 })
